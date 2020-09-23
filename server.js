368dfc9b87c61bb3cbef4498712dbf49aab4bb44
HieuVV <hieuvv@fabbi.io>
2020-09-23 17:38:18 +0700

    add client

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req,res) => {
     res.send('API running');
